846860c78f450fab25e7fba1bbc028c83daa6043
linan <greenmemo@163.com>
2015-05-11 20:13:10 +0800

    extract layout: recv userhead, send status_ack_deliver

--- emwidget/src/com/easemob/widget/chatrow/EMChatRowWidget.java
+++ emwidget/src/com/easemob/widget/chatrow/EMChatRowWidget.java
@@ -2,8 +2,6 @@ package com.easemob.widget.chatrow;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
