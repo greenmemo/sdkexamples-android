703d6ad5ad4f4fc03279eebda9c145eb0d9e5e06
linan <greenmemo@163.com>
2015-12-24 15:15:05 +0800

    hyphenate files

--- ChatDemoUI/hyphenate.sh
+++ ChatDemoUI/hyphenate.sh
@@ -7,13 +7,14 @@
 # migrate sdk
 (
     echo "list files..."
-    filelist=`find res -type f -name "*.xml"`
-    filelist+=`find src -type f -name "*.java"`
-    filelist+= AndroidManifest.xml
+    #filelist=`find res -type f -name "*.xml"`
+    #filelist+=`find src -type f -name "*.java"`
+    #filelist+= AndroidManifest.xml
+    filelist=`grep -l easemob  * -r | grep -v hyphenate.sh`
 
     for file in $filelist; do
         echo $file
-        sed -i "" 's/easemob/hyphenate/g' $file
+        gsed -i 's/easemob/hyphenate/g' $file
     done
 
     mv src/com/easemob src/com/hyphenate
