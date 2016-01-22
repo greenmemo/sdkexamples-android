54d036ada31765da8a0ee94798b035537a4b9a09
zhanw <258653667@qq.com>
2016-01-22 17:25:30 +0800

    update for new error code

--- ChatDemoUI/src/com/hyphenate/chatuidemo/adapter/MessageAdapter.java
+++ ChatDemoUI/src/com/hyphenate/chatuidemo/adapter/MessageAdapter.java
@@ -662,7 +662,7 @@ public class MessageAdapter extends BaseAdapter{
             public void run() {
                 if (message.status() == EMMessage.Status.FAIL) {
 
-                    if (message.getError() == EMError.MESSAGE_INCLUDE_ILLEGAL_SPEECH) {
+                    if (message.getError() == EMError.MESSAGE_INCLUDE_ILLEGAL_CONTENT) {
                         Toast.makeText(activity,activity.getString(R.string.send_fail) + activity.getString(R.string.error_send_invalid_content), 0).show();
                     } else if (message.getError() == EMError.GROUP_NOT_JOINED) {
                         Toast.makeText(activity,activity.getString(R.string.send_fail) + activity.getString(R.string.error_send_not_in_the_group), 0).show();
